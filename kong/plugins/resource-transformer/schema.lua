f10f15370ed9402b61e960267c38bdb45e7b5654
Jin Nguyen <dangtrinhnt@mymusictaste.com>
2020-03-18 14:58:23 +0900

    feat(kong-1.5.1): Add empty schema

--- kong/plugins/resource-transformer/schema.lua
+++ kong/plugins/resource-transformer/schema.lua
@@ -1,15 +1,34 @@
--- schema.lua
 local typedefs = require "kong.db.schema.typedefs"
+
 
 return {
   name = "resource-transformer",
   fields = {
-    { consumer = typedefs.no_consumer },
-    { run_on = typedefs.run_on_first },
-    { protocols = typedefs.protocols_http },
-    { config = {
+    {
+      -- this plugin will only be applied to Services or Routes
+      consumer = typedefs.no_consumer
+    },
+    {
+      -- this plugin will only be executed on the first Kong node
+      -- if a request comes from a service mesh (when acting as
+      -- a non-service mesh gateway, the nodes are always considered
+      -- to be "first".
+      run_on = typedefs.run_on_first
+    },
+    {
+      -- this plugin will only run within Nginx HTTP module
+      protocols = typedefs.protocols_http
+    },
+    {
+      config = {
         type = "record",
         fields = {
-    }, }, },
+          -- Describe your plugin's configuration's schema here.        
+        },
+      },
+    },
+  },
+  entity_checks = {
+    -- Describe your plugin's entity validation rules
   },
 }
